8fbe2ec7204d50861d11477ff2c55dde855d2c64
Simone <Simone@DESKTOP-O1JN5MC.lan>
2016-07-20 19:12:35 +0200

    Aggiunta dei bean

--- ConnessioneGmail/src/main/java/it/xeno/core/MailConnector.java
+++ ConnessioneGmail/src/main/java/it/xeno/core/MailConnector.java
@@ -1,6 +1,7 @@
 package it.xeno.core;
 
 import java.util.Properties;
+import java.util.logging.Logger;
 
 import javax.mail.Folder;
 import javax.mail.Message;
@@ -9,10 +10,12 @@ import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
 
+import it.xeno.bean.MailConnectorBean;
 import it.xeno.util.PropertyManager;
 
-public class CheckingMails {
+public class MailConnector {
 	
+	private Logger logger = Logger.getLogger(MailConnector.class.getName());
 	private PropertyManager props = PropertyManager.getInstance();
 	
 
@@ -23,13 +26,13 @@ public class CheckingMails {
 			Properties properties = System.getProperties();
 
 			properties.put("mail.pop3.host", host);
-			properties.put("mail.pop3.port", "995");
+			properties.put("mail.pop3.port", port);
 			properties.put("mail.pop3.starttls.enable", "true");
 			Session emailSession = Session.getDefaultInstance(properties);
 
 
 			// create the POP3 store object and connect with the pop server
-			Store store = emailSession.getStore("imaps");
+			Store store = emailSession.getStore(mailStoreType);
 			store.connect(host, username, password);
 			
 			Folder folder = store.getFolder("INBOX");
@@ -62,7 +65,7 @@ public class CheckingMails {
 	}
 
 	public static void main(String[] args) {
-		CheckingMails cm = new CheckingMails();
+		MailConnector cm = new MailConnector();
 		cm.check();
 
 	}
@@ -71,17 +74,30 @@ public class CheckingMails {
 	private String mailStoreType;
 	private String username;
 	private String password;
+	private String port;
 	
-	public CheckingMails(){
+	public MailConnector(){
 		host = props.getConfig("mail.host");
 		mailStoreType = props.getConfig("mail.mailstoreType");
 		username = props.getConfig("mail.username");
 		password = props.getConfig("mail.password");
+		port = props.getConfig("mail.port");
+		
 		
 		System.out.println(mailStoreType);
 		System.out.println(host);
 		System.out.println(username);
 		System.out.println(password);
+		System.out.println(port);
+		
+	}
+	
+	public MailConnector(MailConnectorBean mcb){
+		host = mcb.getHost();
+		mailStoreType = mcb.getMailStoreType();
+		username = mcb.getUsername();
+		password = mcb.getPassword();
+		port = mcb.getPort();
 		
 	}
 
